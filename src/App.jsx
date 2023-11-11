dfac99f9d52fb0aebe8bbc0801b22757e1904d61
MAO <mjose.araya@outlook.com>
2023-11-10 22:02:49 -0300

    Base-colaboradores2

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,23 @@ import Alert from './components/Alert';
 import Buscador from './components/Buscador';
 import Formulario from './components/Formulario';
 import Listado from './components/Listado';
-import { BaseColaboradores } from './components/BaseColaboradores';
 import { colaboradores } from './colaboradores';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
 
   return (
-    <BaseColaboradores colaboradores={colaboradores}>
-   
-    </BaseColaboradores>
-  )
+    <>
+
+      <Alert />
+      <Buscador />
+      <Formulario />
+      <Listado colaboradores={colaboradores} />
+</>
+  );
 }
 
-export default App
+export default App;
--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,22 +1,33 @@
+import React from 'react';
 import Table from 'react-bootstrap/Table';
 
-function Listado({colaboradores}) {
-  return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>id</th>
-          <th>nombre</th>
-          <th>correo</th>
-          <th>edad</th>
-          <th>cargo</th>
-          <th>telefono</th>
-
-        </tr>
-      </thead>
-      
-    </Table>
-  );
-}
-
-export default BasicExample;
+const Listado = ({}) => {
+    return (
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>nombre</th>
+            <th>correo</th>
+            <th>edad</th>
+            <th>cargo</th>
+            <th>telefono</th>
+          </tr>
+        </thead>
+        <tbody>
+          {colaboradores.map((colaborador) => (
+            <tr key={colaborador.id}>
+              <td>{colaborador.id}</td>
+              <td>{colaborador.nombre}</td>
+              <td>{colaborador.correo}</td>
+              <td>{colaborador.edad}</td>
+              <td>{colaborador.cargo}</td>
+              <td>{colaborador.telefono}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    );
+  }
+  
+  export default Listado;
